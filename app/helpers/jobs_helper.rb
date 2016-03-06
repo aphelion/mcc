844917a1a0f2c6682ae8ae5871e3fd404e176bdb
Leonhardt Koepsell <leonhardt@aphelion.io>
2016-03-06 14:48:36 +0100

    Show Job status label with color
    [#115100563]

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,12 +1,21 @@
 module JobsHelper
-  def display_status_class(job)
-    case job.status
-      when 'passed'
-        'card-success'
-      when 'failed'
-        'card-danger'
-      else
-        ''
-    end
+  STATUS_HASH = {
+      passed: 'success',
+      failed: 'danger'
+  }.with_indifferent_access
+
+  def job_card_class(job)
+    color = job_status_css_color(job.status)
+    color ? 'card-' + color : ''
   end
+
+  def job_status_label_class(job)
+    color = job_status_css_color(job.status)
+    'label-' + (color ? color : 'default')
+  end
+
+  def job_status_css_color(status)
+    STATUS_HASH[status]
+  end
+
 end
--- spec/views/jobs/_job.html.haml_spec.rb
+++ spec/views/jobs/_job.html.haml_spec.rb
@@ -1,25 +1,61 @@
 describe 'jobs/_job.html.haml' do
   fixtures(:jobs)
-  let(:job) { jobs('job_1') }
 
-  before do
+  def do_render(job)
     fulfill '_job renders job'
     render partial: 'job', locals: {job: job}
   end
 
-  it 'shows the Job data' do
-    contract 'job.name -> ""'
-    contract 'job.status -> ""'
+  context 'any Job' do
+    let(:job) { jobs('job_1') }
 
-    expect(rendered).to include(job.name)
-    expect(rendered).to include(job.status.humanize(capitalize: false))
+    before do
+      do_render(job)
+    end
+
+    describe 'show Job information' do
+      it 'shows the Job name' do
+        contract 'job.name -> ""'
+        expect(rendered).to include(job.name)
+      end
+
+      it 'shows the Job status' do
+        contract 'job.status -> ""'
+        expect(rendered).to include(job.status.humanize(capitalize: false))
+      end
+    end
+
+    describe 'Job links' do
+      it 'links to the Job edit page' do
+        assert_select 'a', 'Edit', href: edit_job_path(job)
+      end
+
+      it 'links to the Job display page' do
+        assert_select 'a', 'Display', href: display_job_path(job)
+      end
+    end
   end
 
-  it 'links to the Job edit page' do
-    assert_select 'a', 'Edit', href: edit_job_path(job)
-  end
+  describe 'status colors' do
+    context 'status is never run' do
+      it 'shows the status in a grey label' do
+        do_render(jobs('never_run'))
+        assert_select '.label-default'
+      end
+    end
 
-  it 'links to the Job display page' do
-    assert_select 'a', 'Display', href: display_job_path(job)
+    context 'status is passed' do
+      it 'shows the status in a green label' do
+        do_render(jobs('passed'))
+        assert_select '.label-success'
+      end
+    end
+
+    context 'status is failed' do
+      it 'shows the status in a red label' do
+        do_render(jobs('failed'))
+        assert_select '.label-danger'
+      end
+    end
   end
 end
